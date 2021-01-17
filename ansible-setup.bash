03df0eccc389b80d1c79172e66b332336ff534b2
Yavar <yavar.azarafra@gmail.com>
2021-01-18 01:15:13 +0330

    Create ansible-setup.bash

---
+++ ansible-setup.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "###################Installing PIP###################"
+echo -e "\n"
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+$(which python3) get-pip.py
+echo -e "\n"
+
+echo "#######################DONE!#######################"
+echo -e "\n"
+echo "#################Installing Ansible#################"
+echo -e "\n"
+
+$(which python3) -m pip install ansible pyvmomi paramiko
+
+echo -e "\n"
+echo "#######################DONE!#######################"
