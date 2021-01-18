4a2a038df7f369a1f4ec232f84158940e90accba
Yavar <yavar.azarafra@gmail.com>
2021-01-18 17:09:15 +0330

    Update ansible-setup.sh

--- ansible-setup.sh
+++ ansible-setup.sh
@@ -12,7 +12,15 @@ echo -e "\n"
 echo "#################Installing Ansible#################"
 echo -e "\n"
 
-$(which python3) -m pip install ansible pyvmomi paramiko
+$(which python3) -m pip install ansible pyvmomi paramiko setuptools testresources
+
+echo -e "\n"
+echo "#######################DONE!#######################"
+echo -e "\n"
+echo "#################Installing vSphere Automation SDK#################"
+echo -e "\n"
+
+$(which python3) -m pip --upgrade git+https://github.com/vmware/vsphere-automation-sdk-python.git
 
 echo -e "\n"
 echo "#######################DONE!#######################"
